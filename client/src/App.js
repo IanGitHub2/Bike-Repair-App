709df685e493f15232f30783cee308788ada310d
Ian Kincaid <ikbusinessacct@gmail.com>
2020-02-10 00:53:58 -0500

    home page route set up

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import CreateOrderForm from './components/CreateOrderForm'
 import './App.css';
 import CreateRepairForm from './components/CreateRepairForm';
 import CreateBicycleForm from './components/CreateBicycleForm';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route exact path="/home-page" component={HomePage}/>
           <Route exact path="/create-order" component={CreateOrderForm}/>
           <Route exact path="/create-repair" component={CreateRepairForm}/>
           <Route exact path="/create-bicycle" component={CreateBicycleForm}/>
