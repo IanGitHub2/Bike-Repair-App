e4610ec9c28d686b2e96be00a6c934c8f17f8852
Ian Kincaid <ikbusinessacct@gmail.com>
2020-02-10 00:51:47 -0500

    home page set up

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import OrderList from './OrderList'
+
+export default class HomePage extends React.Component {
+    state = {
+        orderList: [
+            {
+                pick_date: '',
+                order_name: '',
+                phone_number: '',
+                email: '',
+                special_request: '',
+                repair: null,
+                bicycle: null
+            }
+        ]
+    }
+
+    componentDidMount(){
+        axios.get('/api/v1/post')
+        .then((res) => {
+            this.setState({ orderList: res.data })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <h1>Place and order</h1>
+                    <Link to={`/create-order`}>
+                        <button>Place Order</button>
+                    </Link>
+                </div>
+                <div>
+                    <OrderList orderList={this.state.orderList}/>
+                </div>
+            </div>
+        )
+    }
+}
