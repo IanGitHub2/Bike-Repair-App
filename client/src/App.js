df84d9227e0ed61d545f78d0205bbcaf756bd590
Ian Kincaid <ikbusinessacct@gmail.com>
2020-02-09 21:54:59 -0500

    app.js routes added for create form

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import CreateOrderForm from './components/CreateOrderForm'
 import './App.css';
+import CreateRepairForm from './components/CreateRepairForm';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/create-order" component={CreateOrderForm}/>
+          <Route exact path="/create-repair" component={CreateRepairForm}/>
+          <Route exact path="/create-bicycle" component={CreateBicycleForm}/>
         </Switch>
       </Router>
       <p>Hi</p>
