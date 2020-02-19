f0d63d8e57f56c2dae39f2bd81d3bd406968aaf2
Ian Kincaid <ikbusinessacct@gmail.com>
2020-02-19 13:31:05 -0500

    name, date, bike, request italized and bolded

--- client/src/components/OrderList.js
+++ client/src/components/OrderList.js
@@ -10,10 +10,10 @@
                  {this.props.orderList.map((order, i) => {
                      return(
                          <div class="order-name-date">
-                             <div>Name: {order.order_name},</div>
-                             <div>Drop off: {order.pick_date},</div>
-                             <div>Bicycle: {order.bicycle},</div>
-                             <div>Special request: {order.special_request}</div>
+                             <div><em><strong>Name:</strong></em> {order.order_name}</div>
+                             <div><em><strong>Drop off:</strong></em> {order.pick_date}</div>
+                             <div><em><strong>Bicycle:</strong></em> {order.bicycle}</div>
+                             <div><em><strong>Special request:</strong></em> {order.special_request}</div>
                          </div>
                      )
                  })}
